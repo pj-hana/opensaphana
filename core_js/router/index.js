01caca1566dcb230cf1fd183c6baef0328e7f908
XSA_DEV <XSA_DEV@unknown>
2018-02-15 03:10:00 +0000

    fads

--- core_js/router/index.js
+++ core_js/router/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
-module.exports = function(app,server){
-	app.use("/node",require("./routes/myNode")());
-};                          
+module.exports = function(app, server) {
+	app.use("/node", require("./routes/myNode")());
+	app.use("/node/excAsync",require("./routes/exerciseAsync")(server));
+	
+};
--- core_js/router/routes/myNode.js
+++ core_js/router/routes/myNode.js
@@ -3,10 +3,70 @@
 
 var express = require("express");
 
-module.exports = function(){
+module.exports = function() {
 	var app = express.Router();
-	app.get("/", function(req,res){
+	var async = require("async");
+
+	app.get("/", function(req, res) {
 		res.send("Hello from Node.js");
 	});
+
+	app.get("/example1", function(req, res) {
+		var client = req.db;
+		client.prepare(
+			"select SESSION_USER from \"DUMMY\" ",
+			function(err, statement) {
+				if (err) {
+					res.type("text/plain").status(500).send("ERROR: " +
+						err.toString());
+					return;
+				}
+				statement.exec([],
+					function(err, results) {
+						if (err) {
+							res.type("text/plain").status(500).send("ERROR: " +
+								err.toString());
+							return;
+						} else {
+							var result = JSON.stringify({
+								Objects: results
+							});
+							res.type("application/json").status(200).send(result);
+						}
+					});
+			});
+	});
+	app.get("/example2", function(req, res) {
+		var client = req.db;
+		async.waterfall([
+			function prepare(callback) {
+				client.prepare(
+					"select SESSION_USER from \"DUMMY\" ",
+					function(err, statement) {
+						callback(null, err, statement);
+					});
+			},
+			function execute(err, statement, callback) {
+				statement.exec([], function(execErr, results) {
+					callback(null, execErr, results);
+				});
+			},
+			function response(err, results, callback) {
+				if (err) {
+					res.type("text/plain").status(500).send("ERROR: " +
+						err.toString());
+					return;
+				} else {
+					var result =
+						JSON.stringify({
+							Objects: results
+						});
+					res.type("application/json").status(200).send(result);
+				}
+				callback();
+			}
+		]);
+	});
+
 	return app;
 };
